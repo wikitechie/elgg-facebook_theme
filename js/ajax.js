32dcc201876fc2588e7064982948f342354c492c
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-09-22 23:46:59 +0300

    Fixed some js conflicts

--- js/ajax.js
+++ js/ajax.js
@@ -52,36 +52,4 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	$("form.elgg-form-comments-add").ajaxForm({
-		beforeSubmit: function(arr,formObj,options) {
-			$(formObj).before($("<div class='elgg-ajax-loader' ></div>"));
-		},
-		success: function(responseText, statusText, xhr, formObj) {
-			var guid = $(formObj).find("input[name='entity_guid']").val();
-			elgg.view('annotation/getannotations',{
-				data:{
-					'guid':guid,
-					'limit': 1,
-					'annotation_name': 'generic_comment'
-				},
-				'formObj':formObj,
-				success:function(data) {
-					$(this.formObj).prev().remove();
-					if ($(formObj).prev().length == 0) {
-						$(formObj).before($(data));
-					}
-					else {
-						data = $(data).children().first();
-						$(formObj).prev().append(data);
-					}
-					$(formObj).find("input[type='text']").val("");
-						
-				}
-			});
-		},
-		error: function() {
-			$(this).removeClass("elg-ajax-loader");
-			//TODO Make error logic
-		}
-	});
 });
